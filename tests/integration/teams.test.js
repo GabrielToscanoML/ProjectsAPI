72bdeebf6f1887768efce5a78f8abe2bc5c5b712
Gabriel Toscano <gabrieltoscano1@outlook.com>
2023-10-20 08:15:22 -0300

    Instalando Sinon em ambiente de desenvolvimento e criação de testes 'duble' criado

--- tests/integration/teams.test.js
+++ tests/integration/teams.test.js
@@ -1,40 +1,71 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const fs = require('fs');
 const app = require('../../src/app');
 
-const getResult = {
-    teams: [
-      {
-        id: 1,
-        name: 'São Paulo Futebol Clube',
-        initials: 'SPFC',
-      },
-      {
-        id: 2,
-        name: 'Clube Atlético Mineiro',
-        initials: 'CAM',
-      },
-      {
-        id: 3,
-        name: 'Flamengo',
-        initials: 'FLA',
-      },
-      {
-        id: 4,
-        name: 'Fortaleza',
-        initials: 'FOR',
-      },
-    ],
-  };
+const mockFile = JSON.stringify({ 
+  teams: [
+    {
+      id: 1,
+      name: 'São Paulo Futebol Clube',
+      initials: 'SPFC',
+    },
+    {
+      id: 2,
+      name: 'Clube Atlético Mineiro',
+      initials: 'CAM',
+    },
+    {
+      id: 3,
+      name: 'Flamengo',
+      initials: 'FLA',
+    },
+    {
+      id: 4,
+      name: 'Fortaleza',
+      initials: 'FOR',
+    },
+  ],
+});
+  
+const output = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPFC',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+  {
+    id: 3,
+    name: 'Flamengo',
+    initials: 'FLA',
+  },
+  {
+    id: 4,
+    name: 'Fortaleza',
+    initials: 'FOR',
+  },
+];
 
 const { expect } = chai;
 
 chai.use(chaiHttp);
 
-describe('Usando o método GET em /teams', () => {
-    it('Retorna a lista completa dos times com status 200!', async () => {
+describe('Testando a API Teams', () => {
+  // sinon usado para "dublar" uma requisição usando mock
+  beforeEach(() => sinon.stub(fs.promises, 'readFile').resolves(mockFile));
+  afterEach(() => sinon.restore());
+
+  describe('Usando o método GET em /teams', () => {
+    it('Retorna a lista completa dos times com status 200', async () => {
         const response = await chai.request(app).get('/teams');
         expect(response.status).to.be.equal(200);
-        expect(response.body).to.deep.equal(getResult);
+        expect(response.body.teams).to.deep.equal(output);
     });
+  });
 });
